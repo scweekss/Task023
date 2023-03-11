86d09a9187a885a28b0eb1b0c11229b225a1bd46
scweekkkkk <124443824+scweekss@users.noreply.github.com>
2023-03-11 17:43:11 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Задача 23
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+//оператор инкремента ++ для увел. на 1
+
+
+int number = Prompt("Введите натуральное число");
+
+if (number>0) PrintSqr (number);
+else Console.WriteLine("Введено не натуральное число");
+
+void PrintSqr(int n)
+{
+    Console.WriteLine("Число     Квадрат числа");
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i,5} {i*i*i, 5}");
+    }
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int cc = Convert.ToInt32(Console.ReadLine());
+    return cc;
+}
